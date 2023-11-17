680d811534326a97e0cd9edd7853a29725875c5c
Oscar-CR <krz.oficial@gmail.com>
2023-11-17 00:46:28 -0600

    agregado de liberia flowbite y avance crud ambitos

---
+++ app/Http/Controllers/AmbitController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ambit;
+use Illuminate\Http\Request;
+
+class AmbitController extends Controller
+{
+    public function store(Request $request) {
+        $create_ambit = new Ambit();
+        $create_ambit->name = $request->name;
+        $create_ambit->description = $request->description;
+        $create_ambit->period_id =1;
+        $create_ambit->save();
+
+        return 1;
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AmbitController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -23,7 +24,10 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
 
-    
+    Route::post('/ambit/store', [AmbitController::class, 'store'])->name('ambit.store');
+
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
